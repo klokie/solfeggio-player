1740de439fb49534f2792cb7e7afbfc632bb8290
Daniel Grossfeld <github@klokie.com>
2024-05-23 22:28:47 +0200

    inital commit

---
+++ src/components/AudioContextProvider.tsx
@@ -0,0 +1,25 @@
+import React, { createContext, useContext, useRef } from "react";
+
+interface IAudioContext {
+  audioContext: AudioContext | null;
+}
+
+const AudioContextState = createContext<IAudioContext>({ audioContext: null });
+
+export const useAudioContext = () => useContext(AudioContextState);
+
+const AudioContextProvider: React.FC = ({ children }) => {
+  const audioRef = useRef<AudioContext | null>(null);
+
+  if (!audioRef.current) {
+    audioRef.current = new AudioContext();
+  }
+
+  return (
+    <AudioContextState.Provider value={{ audioContext: audioRef.current }}>
+      {children}
+    </AudioContextState.Provider>
+  );
+};
+
+export default AudioContextProvider;
---
+++ src/components/FrequencyPlayer.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useAudioContext } from "./AudioContextProvider";
+
+interface FrequencyPlayerProps {
+  frequency: number;
+  label: string;
+}
+
+const FrequencyPlayer: React.FC<FrequencyPlayerProps> = ({
+  frequency,
+  label,
+}) => {
+  const { audioContext } = useAudioContext();
+
+  const playFrequency = () => {
+    if (audioContext) {
+      const oscillator = audioContext.createOscillator();
+      oscillator.frequency.value = frequency;
+      oscillator.connect(audioContext.destination);
+      oscillator.start();
+      setTimeout(() => {
+        oscillator.stop();
+      }, 10000); // stop after 10 seconds
+    }
+  };
+
+  return <button onClick={playFrequency}>Play {label}</button>;
+};
+
+export default FrequencyPlayer;
